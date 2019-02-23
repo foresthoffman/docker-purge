992a05cc02578e521a55c4ad4c7ed8163f496b4f
Forest Hoffman <forestjhoffman@gmail.com>
2019-02-23 17:00:55 -0600

    Add options to limit removal
    - Add `-n` option to delete containers that match the provided name pattern.
    - Add `-i` option to remove images. Images are no longer removed by default.

--- purge.sh
+++ purge.sh
@@ -1,12 +1,68 @@
 #!/bin/bash
-# Removes all docker containers and images.
+# Removes docker containers and images.
 #
 # Author: Forest Hoffman <github.com/foresthoffman>
 # All rights reserved.
 #
 
-echo "Removing all Docker containers."
-docker ps -a | awk -F' ' '/[a-z0-9]+/{ print $1 }' | xargs -I % docker rm -f %
+excludeImagePattern="example"
+options=('-n' '--name' '-i' '--images' '-h' '--help')
 
-echo "Removing all Docker images."
-docker images -a | awk -F' ' '/[a-z0-9]+/{ print $3 }' | xargs -I % docker rmi -f %
+error() {
+	echo "ERROR: $1"
+}
+
+usage() {
+cat << END
+⚡️ Docker Purge⚡️
+Remove Docker containers and images.
+
+Usage: docker-purge [options]
+
+${options[0]}, ${options[1]} [pattern]	Purge containers matching the provided name pattern, default: '.*'.
+${options[2]}, ${options[3]}	Purge images as well as containers, default: OFF (Ignores '${options[1]}' option).
+${options[4]}, ${options[5]}	This information.
+END
+}
+
+namePattern=""
+allImages=false
+index=0
+for arg in $@; do
+	index=$(echo "$index+1" | bc)
+	# help menu
+	if [[ "$arg" == "${options[4]}" ]] || [[ "$arg" == "${options[5]}" ]]; then
+		usage
+		exit 0
+	fi
+	# by name
+	if [[ "$arg" == "${options[0]}" ]] || [[ "$arg" == "${options[1]}" ]]; then
+		next=$(echo $index+1 | bc)
+		if [[ "${!next}" == "" ]] || [[ "$(echo ${!next} | grep -E '^-')" == "${!next}" ]]; then
+			error "A valid pattern is required for the '${options[1]}' option."
+			usage
+			exit 1
+		fi
+		namePattern="${!next}"
+		continue
+	fi
+	# all images
+	if [[ "$arg" == "${options[2]}" ]] || [[ "$arg" == "${options[3]}" ]]; then
+		allImages=true
+		continue
+	fi
+done
+
+removeStr="Removing all Docker containers"
+if [[ "${namePattern}" != "" ]]; then
+	echo "${removeStr} with names matching '${namePattern}'."
+	docker ps --filter name="${namePattern}" --format "{{.Names}}" | xargs -I '%' docker rm -f %
+else
+	echo "${removeStr}."
+	docker ps -a --format "{{.Names}}" | xargs -I '%' docker rm -f %
+fi
+
+if $allImages; then
+	echo "Removing all Docker images."
+	docker images -a | awk -F' ' "/$excludeImagePattern/{ next } /[a-z0-9]+/{ print \$3 }" | xargs -I % docker rmi -f %
+fi
